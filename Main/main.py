153f12b0cfd0adb8999957f3ce7fb4a36f8aca8c
Anthony DiPilato <anthony@bumbol.com>
2016-12-24 21:37:59 -0500

    fix drive location

--- Main/main.py
+++ Main/main.py
@@ -94,7 +94,7 @@ root.focus_set() # <-- move focus to this widget
 
 # used for final encoded output
 # location of the Flash Drive 
-mediaDirectory = "/dev/sda1/Media"
+mediaDirectory = '/media/pi/Media'
 
 
 # widgets
